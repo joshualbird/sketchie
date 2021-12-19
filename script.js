d9b7ca0af325295a861c0178a566da3e2c2b8f5a
dribby <joshualbird@live.com>
2021-12-19 12:51:21 -0500

    updated visuals and functionality

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ initializeGrid();
 
 function initializeGrid(sketchies){
     sketchies = Number(prompt('How many Sketchie boxes per side? (1-100)','16'));  
-    let boxWidth = (96 / sketchies);
+    let intViewportWidth = window.innerWidth / 2;
+    let boxWidth = (intViewportWidth / sketchies);
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     
     for (i = 0; i < sketchies; i++){
         for (j =0; j < sketchies; j++){
             const pixelBox = document.createElement('div');
             
-            pixelBox.style.width = boxWidth + "vmin";
-            pixelBox.style.height = boxWidth + "vmin";
+            pixelBox.style.width = boxWidth + "px";
+            pixelBox.style.height = boxWidth + "px";
             
             pixelBox.classList.add('pixel');
             pixelBox.addEventListener('mouseover', function(e){
@@ -29,6 +30,10 @@ function clearGrid(){
     elements = document.getElementsByClassName('pixel');
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor="white";
+        
+        elements[i].addEventListener('mouseover',function(e){
+            e.target.style.backgroundColor = 'black';
+        });
     }
 
 }
@@ -49,15 +54,10 @@ function rainbowGrid(){
 }
 
 
-var clearButton = document.getElementsByClassName("clear");
-
-for (var i = 0; i < clearButton.length; i++) {
-    clearButton[i].addEventListener('click', clearGrid, false);
-}
+var clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', clearGrid, false);
 
 
-var rainbowButton = document.getElementsByClassName("rainbow");
 
-for (var i = 0; i < rainbowButton.length; i++) {
-    rainbowButton[i].addEventListener('click', rainbowGrid, false);
-}
+var rainbowButton = document.querySelector(".rainbow");
+rainbowButton.addEventListener('click', rainbowGrid, false);
